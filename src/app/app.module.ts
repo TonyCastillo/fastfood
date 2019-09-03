5fa76fe9d87f2411f931695dafda6e15e3b53a7f
TonyCastillo <tony.castillo.7@gmail.com>
2019-09-03 19:16:14 -0400

    ultimos cambios para la vista dash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { LoginComponent } from './components/login/login.component';
     AppComponent,
     CardsComponent,
     LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
